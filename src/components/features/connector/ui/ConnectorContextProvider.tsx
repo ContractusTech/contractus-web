a49df768fa8d68a7693b5daaf3f77c4e9d06851f
Ilya Bakirov <everjupe13@gmail.com>
2023-11-12 18:17:03 +0300

    fix: fix connection / publicKey

--- src/components/features/connector/ui/ConnectorContextProvider.tsx
+++ src/components/features/connector/ui/ConnectorContextProvider.tsx
@@ -1,11 +1,10 @@
 'use client'
 
-import { createTheme, StyledEngineProvider, ThemeProvider } from '@mui/material'
-import { deepPurple, pink } from '@mui/material/colors'
-import { WalletModalProvider as AntDesignWalletModalProvider } from '@solana/wallet-adapter-ant-design'
-import type { Adapter, WalletError } from '@solana/wallet-adapter-base'
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
-import { WalletDialogProvider as MaterialUIWalletDialogProvider } from '@solana/wallet-adapter-material-ui'
+import {
+  type Adapter,
+  WalletAdapterNetwork,
+  type WalletError
+} from '@solana/wallet-adapter-base'
 import {
   ConnectionProvider,
   WalletProvider
@@ -24,67 +23,15 @@ import React, { useCallback, useMemo } from 'react'
 
 import { AutoConnectProvider, useAutoConnect } from './AutoConnectProvider'
 
-const theme = createTheme({
-  palette: {
-    mode: 'dark',
-    primary: {
-      main: deepPurple[700]
-    },
-    secondary: {
-      main: pink[700]
-    }
-  },
-  components: {
-    MuiButtonBase: {
-      styleOverrides: {
-        root: {
-          justifyContent: 'flex-start'
-        }
-      }
-    },
-    MuiButton: {
-      styleOverrides: {
-        root: {
-          textTransform: 'none',
-          padding: '12px 16px'
-        },
-        startIcon: {
-          marginRight: 8
-        },
-        endIcon: {
-          marginLeft: 8
-        }
-      }
-    }
-  }
-})
-
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const { autoConnect } = useAutoConnect()
 
-  // Can be set to 'devnet', 'testnet', or 'mainnet-beta'
   const network = WalletAdapterNetwork.Devnet
 
-  // You can also provide a custom RPC endpoint
   const endpoint = useMemo(() => clusterApiUrl(network), [network])
 
   const wallets = useMemo(
-    () => [
-      /**
-       * Wallets that implement either of these standards will be available automatically.
-       *
-       *   - Solana Mobile Stack Mobile Wallet Adapter Protocol
-       *     (https://github.com/solana-mobile/mobile-wallet-adapter)
-       *   - Solana Wallet Standard
-       *     (https://github.com/solana-labs/wallet-standard)
-       *
-       * If you wish to support a wallet that supports neither of those standards,
-       * instantiate its legacy wallet adapter here. Common legacy adapters can be found
-       * in the npm package `@solana/wallet-adapter-wallets`.
-       */
-      new PhantomWalletAdapter(),
-      new SolflareWalletAdapter()
-    ],
+    () => [new PhantomWalletAdapter(), new SolflareWalletAdapter()],
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [network]
   )
@@ -127,11 +74,7 @@ const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
         onError={onError}
         autoConnect={autoConnect && autoSignIn}
       >
-        <MaterialUIWalletDialogProvider>
-          <AntDesignWalletModalProvider>
-            <ReactUIWalletModalProvider>{children}</ReactUIWalletModalProvider>
-          </AntDesignWalletModalProvider>
-        </MaterialUIWalletDialogProvider>
+        <ReactUIWalletModalProvider>{children}</ReactUIWalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
   )
@@ -139,14 +82,10 @@ const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
 
 export const ContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
   return (
-    <StyledEngineProvider injectFirst>
-      <ThemeProvider theme={theme}>
-        <SnackbarProvider>
-          <AutoConnectProvider>
-            <WalletContextProvider>{children}</WalletContextProvider>
-          </AutoConnectProvider>
-        </SnackbarProvider>
-      </ThemeProvider>
-    </StyledEngineProvider>
+    <SnackbarProvider>
+      <AutoConnectProvider>
+        <WalletContextProvider>{children}</WalletContextProvider>
+      </AutoConnectProvider>
+    </SnackbarProvider>
   )
 }
--- src/components/features/wallet/ui/SignMessage.tsx
+++ src/components/features/wallet/ui/SignMessage.tsx
@@ -1,51 +1,61 @@
 'use client'
 
-import { ed25519 } from '@noble/curves/ed25519'
+// import { ed25519 } from '@noble/curves/ed25519'
+// import { useConnection, useWallet } from '@solana/wallet-adapter-react'
 import { useWallet } from '@solana/wallet-adapter-react'
-import bs58 from 'bs58'
-import type { FC } from 'react'
-import React, { useCallback } from 'react'
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+// import bs58 from 'bs58'
+// import { type FC, useCallback } from 'react'
+import { type FC, useEffect } from 'react'
 
-import { Button } from '@/components/ui/button'
+// import { Button } from '@/components/ui/button'
 import { useNotify } from '@/components/ui/use-notify'
 
 const SignMessage: FC = () => {
+  // const { connection } = useConnection()
   const { publicKey, signMessage } = useWallet()
   const notify = useNotify()
 
-  const onClick = useCallback(async () => {
-    try {
-      if (!publicKey) {
-        throw new Error('Wallet not connected!')
-      }
-      if (!signMessage) {
-        throw new Error('Wallet does not support message signing!')
-      }
+  useEffect(() => {
+    console.log(publicKey)
+  }, [publicKey])
 
-      const message = new TextEncoder().encode(
-        `${
-          window.location.host
-        } wants you to sign in with your Solana account:\n${publicKey.toBase58()}\n\nPlease sign in.`
-      )
-      const signature = await signMessage(message)
+  // const onClick = useCallback(async () => {
+  //   try {
+  //     if (!publicKey) {
+  //       throw new Error('Wallet not connected!')
+  //     }
+  //     if (!signMessage) {
+  //       throw new Error('Wallet does not support message signing!')
+  //     }
 
-      if (!ed25519.verify(signature, message, publicKey.toBytes())) {
-        throw new Error('Message signature invalid!')
-      }
-      notify('success', `Message signature: ${bs58.encode(signature)}`)
-    } catch (error: any) {
-      notify('error', `Sign Message failed: ${error?.message}`)
-    }
-  }, [publicKey, signMessage, notify])
+  //     const message = new TextEncoder().encode(
+  //       `${
+  //         window.location.host
+  //       } wants you to sign in with your Solana account:\n${publicKey.toBase58()}\n\nPlease sign in.`
+  //     )
+  //     const signature = await signMessage(message)
+
+  //     if (!ed25519.verify(signature, message, publicKey.toBytes())) {
+  //       throw new Error('Message signature invalid!')
+  //     }
+  //     notify('success', `Message signature: ${bs58.encode(signature)}`)
+  //   } catch (error: any) {
+  //     notify('error', `Sign Message failed: ${error?.message}`)
+  //   }
+  // }, [publicKey, signMessage, notify])
 
   return (
-    <Button
-      color="primary"
-      onClick={onClick}
-      disabled={!publicKey || !signMessage}
-    >
-      Sign Message
-    </Button>
+    <div>
+      <WalletMultiButton />
+    </div>
+    // <Button
+    //   color="primary"
+    //   onClick={onClick}
+    //   disabled={!publicKey || !signMessage}
+    // >
+    //   Sign Message
+    // </Button>
   )
 }
 
