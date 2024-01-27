8cb452e90a12934defca188f7d9bd66b9e8576f5
Тимерьян Насибуллин <-github-email@example.com>
2024-01-27 23:56:22 +0500

    fix: fixed balance estimare view and add tofixed layput

--- src/components/widgets/estimatedBalance/ui/balance.tsx
+++ src/components/widgets/estimatedBalance/ui/balance.tsx
@@ -1,15 +1,11 @@
-import { useBalance } from '@/api/modules/accounts/hooks/useBalance'
-
-export const Balance = () => {
-  const { balance } = useBalance({ currency: 'USD' })
-
+export const Balance = ({ amount }: { amount: string }) => {
   return (
     <section className="mb-30">
       <div className="mb-13">
         <h1 className="typo-label text-center">Estimate balance</h1>
       </div>
       <h2 className="text-center text-6xl font-light leading-none">
-        {/* ${balance?.estimateAmount} */}
+        ${Number(amount).toFixed(2)}
       </h2>
     </section>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,8 @@ import { useEffect } from 'react'
 import { useDeals } from '@/api/modules/deals/hooks/useDeals'
 import { useUserStore } from '@/app/store/user-store'
 import { NextPageWithLayout } from '@/app/types'
-import { AddCircleIcon } from '@/assets/svg/AddCircleIcon'
 import { ChevronDownIcon } from '@/assets/svg/ChevronDownIcon'
-import { CrownIcon } from '@/assets/svg/CrownIcon'
 import { StatisticsList } from '@/components/entities/statistics'
-import { Button } from '@/components/ui/button'
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -16,7 +13,6 @@ import {
   DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu'
 import { DealCard, EmptyPlaceholder } from '@/components/widgets/deals'
-import { Balance } from '@/components/widgets/estimatedBalance'
 import { CreateDealButton, TokenSwapper } from '@/components/widgets/tokens'
 import LayoutDefault from '@/layouts/default'
 
@@ -43,63 +39,42 @@ const IndexPage: NextPageWithLayout = () => {
     }
   }, [connectedUser?.publicKey, refetch])
 
-  useEffect(() => {
-    // api.accounts.topupCreate()
-    // api.accounts.balanceCreate({ currency: 'USD', tokens: [] })
-    // api.accounts.statisticsList()
-    // api.accounts.loanCreate()
-  }, [])
-
   return (
     <>
-      <>
-        <Balance />
-        <section className="mx-auto mb-30 flex gap-13 md:flex-wrap md:justify-center">
-          <Button variant="secondary">
-            <AddCircleIcon className="mr-4 h-16 w-16" />
-            Buy
-          </Button>
-          <span className="flex h-38 items-center gap-x-8 rounded-[12px] border border-input bg-background px-20 py-10 text-sm font-medium ring-offset-background transition-colors hover:bg-accent hover:text-accent-foreground hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
-            <CrownIcon className="mr-4 h-16 w-16" />
-            Get CTUS Token
-          </span>
-        </section>
+      <TokenSwapper />
 
-        <TokenSwapper />
+      <section className="mb-35">
+        <StatisticsList />
+      </section>
 
-        <section className="mb-35">
-          <StatisticsList />
-        </section>
-
-        <section>
-          <div className="mb-20 flex items-center justify-between">
-            <DropdownMenu>
-              <DropdownMenuTrigger className="group">
-                <h2 className="flex items-center gap-x-8 text-2xl font-medium leading-[1.2]">
-                  Latest
-                  <ChevronDownIcon
-                    className="relative top-[1px] h-16 w-16 transition duration-200 group-data-[state=open]:rotate-180"
-                    aria-hidden="true"
-                  />
-                </h2>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="bg-secondary">
-                <DropdownMenuItem>Not latest</DropdownMenuItem>
-                <DropdownMenuItem>In proccess</DropdownMenuItem>
-                <DropdownMenuItem>Others</DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-            <div>
-              <CreateDealButton onSuccess={refetch} />
-            </div>
+      <section>
+        <div className="mb-20 flex items-center justify-between">
+          <DropdownMenu>
+            <DropdownMenuTrigger className="group">
+              <h2 className="flex items-center gap-x-8 text-2xl font-medium leading-[1.2]">
+                Latest
+                <ChevronDownIcon
+                  className="relative top-[1px] h-16 w-16 transition duration-200 group-data-[state=open]:rotate-180"
+                  aria-hidden="true"
+                />
+              </h2>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="bg-secondary">
+              <DropdownMenuItem>Not latest</DropdownMenuItem>
+              <DropdownMenuItem>In proccess</DropdownMenuItem>
+              <DropdownMenuItem>Others</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <div>
+            <CreateDealButton onSuccess={refetch} />
           </div>
-          <div className="grid grid-cols-3 gap-8 md:grid-cols-2" ref={parent}>
-            {deals && deals.map(deal => <DealCard deal={deal} key={deal.id} />)}
+        </div>
+        <div className="grid grid-cols-3 gap-8 md:grid-cols-2" ref={parent}>
+          {deals && deals.map(deal => <DealCard deal={deal} key={deal.id} />)}
 
-            {deals && deals.length === 0 && <EmptyPlaceholder />}
-          </div>
-        </section>
-      </>
+          {deals && deals.length === 0 && <EmptyPlaceholder />}
+        </div>
+      </section>
     </>
   )
 }
