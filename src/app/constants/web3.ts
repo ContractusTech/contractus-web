d6df754538021ac870a530d9b8f9201d817d0182
Simon Hudishkin <saman1303@gmail.com>
2024-02-12 22:19:28 +0500

    Fixed env

--- src/app/constants/web3.ts
+++ src/app/constants/web3.ts
@@ -4,7 +4,7 @@ const MESSAGES = {
   SIGN_MESSAGE: 'web-client',
   WC_ID: ENV.WC_ID,
   WBNB_ADDRESS: '0xae13d989dac2f0debff460ac112a837c89baa7cd',
-  CONTRACTOR_ADDRESS: ENV.CONTRACTOR_ADDRESS
+  CONTRACTOR_ADDRESS: ENV.CONTRACTOR_ADDRESS as `0x${string}`
 } as const
 
 export default MESSAGES
