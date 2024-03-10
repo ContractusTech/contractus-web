2761d6ce1770b1924049ca0a1f7a98e5bc354202
Тимерьян Насибуллин <-github-email@example.com>
2024-03-10 23:33:04 +0500

    feat: add telegram support btn in header

--- src/app/constants/pages.ts
+++ src/app/constants/pages.ts
@@ -5,5 +5,7 @@ export const PAGES = {
   TERMS: 'https://files.contractus.tech/Contractus-Terms.pdf',
   PRIVACY: 'https://files.contractus.tech/Contractus-PrivacyPolicy.pdf',
   GITHUB: 'https://github.com/ContractusTech',
-  HOME: 'https://contractus.tech/'
+  HOME: 'https://contractus.tech/',
+
+  SUPPORT: 'https://t.me/ContractusSupportBot'
 }
